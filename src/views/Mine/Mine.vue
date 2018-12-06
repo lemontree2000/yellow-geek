fcd47f5d01149799323c26f8739d33b15cf978de
lemontree2000 <gonelove.guo@gmail.com>
2018-12-06 23:57:45 +0800

    update

--- src/views/Mine/Mine.vue
+++ src/views/Mine/Mine.vue
@@ -10,19 +10,37 @@
           <img src alt>
         </div>
         <div class="name-width-seecount">
-          <h1>
-            即刻鸡客
-          </h1>
-          <p>
-            11人来看过你
-          </p>
+          <h1>即刻鸡客</h1>
+          <p>11人来看过你</p>
         </div>
       </section>
       <ul class="yg-mine-list">
-
+        <li>
+          <img src alt>
+          <span>我关注的主题</span>
+        </li>
+        <li>
+          <img src alt>
+          <span>我的收藏</span>
+        </li>
+        <li>
+          <img src alt>
+          <span>我的通知</span>
+        </li>
+        <li>
+          <img src alt>
+          <span>帮助与反馈</span>
+        </li>
       </ul>
       <ul class="yg-mine-list">
-
+        <li>
+          <img src alt>
+          <span>我创建的主题</span>
+        </li>
+        <li>
+          <img src alt>
+          <span>免流量</span>
+        </li>
       </ul>
     </section>
   </section>
@@ -57,4 +75,22 @@ export default class Home extends Vue {
 
 </script>
 <style lang='less'  scoped>
+@import url("~@/styles/mixin.less");
+.yg-mine {
+  header {
+    height: 44px;
+    background-color: @brand-primary;
+    padding: 0 15px;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    span {
+      width: 24px;
+      height: 24px;
+      background-image: url("./image/ic_navbar_setting@3x.png");
+      background-repeat: no-repeat;
+      background-size: cover;
+    }
+  }
+}
 </style>
