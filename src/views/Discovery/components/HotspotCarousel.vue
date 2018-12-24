a42cd8b272d38ebdec34e4cef1ce113902c4536f
lemontree2000 <gonelove.guo@gmail.com>
2018-12-24 23:57:46 +0800

    add image

--- src/views/Discovery/components/HotspotCarousel.vue
+++ src/views/Discovery/components/HotspotCarousel.vue
@@ -1,22 +1,38 @@
 <!-- 热门活动轮播图 -->
 <template>
-  <div class="discovery-carousel">
-    discovery-carousel
+  <div class="discovery-carousel">discovery-carousel
     <section>
       <swiper>
         <swiperSlide>
           <section class="carousel-item">
-            <img src="" alt="">
+            <img
+              src="https://hdcdn.ruguoapp.com/FgV31-yrxbsAh4dQUKfFRTmtsvJN.png?imageView2/0/w/720/h/360"
+              alt
+            >
           </section>
         </swiperSlide>
-         <swiperSlide>
+        <swiperSlide>
           <section class="carousel-item">
-            <img src="" alt="">
+            <img
+              src="https://hdcdn.ruguoapp.com/Fovyo1w3lU8lNxQsANjNK-UbaM6g.png?imageView2/0/w/720/h/360"
+              alt
+            >
           </section>
         </swiperSlide>
-         <swiperSlide>
+        <swiperSlide>
           <section class="carousel-item">
-            <img src="" alt="">
+            <img
+              src="https://hdcdn.ruguoapp.com/FvlumgsJiOejUCBmU--BCQyvJvJ2.jpg?imageView2/0/w/720/h/360"
+              alt
+            >
+          </section>
+        </swiperSlide>
+        <swiperSlide>
+          <section class="carousel-item">
+            <img
+              src="https://hdcdn.ruguoapp.com/Fj5pUR9Qun5EB6EU9aPU_4YJ1Azj.png?imageView2/0/w/720/h/360"
+              alt
+            >
           </section>
         </swiperSlide>
       </swiper>
@@ -29,7 +45,7 @@ import { Component, Vue } from "vue-property-decorator";
 import { swiper, swiperSlide } from "vue-awesome-swiper";
 
 @Component({
-  components: {swiper, swiperSlide},
+  components: { swiper, swiperSlide },
 })
 export default class Home extends Vue {
   // @Prop({ default: 'default value' }) propB!: string
@@ -53,5 +69,4 @@ export default class Home extends Vue {
 }
 </script>
 <style lang='less'  scoped>
-
 </style>
